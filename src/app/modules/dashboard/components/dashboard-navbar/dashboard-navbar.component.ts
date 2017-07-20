ccba07746d5a8435ac48e009cc9f61692cb2f09f
Stephen Wright <sjw948@gmail.com>
2017-07-20 08:18:06 +0100

    added navbar toggle for top-level menu

--- src/app/modules/dashboard/components/dashboard-navbar/dashboard-navbar.component.ts
+++ src/app/modules/dashboard/components/dashboard-navbar/dashboard-navbar.component.ts
@@ -9,7 +9,32 @@ export class DashboardNavbarComponent implements OnInit {
 
   constructor() { }
 
+  navbarVisibility = false;
+
   ngOnInit() {
+  }
+
+  resizeNavbarVisibility() {
+    if(screen.width >= 1024) {
+      this.navbarVisibility = true;
+    } else {
+      this.navbarVisibility = false;
+    }
+  }
+
+  toggleNavbarVisibility() {
+    this.navbarVisibility = !this.navbarVisibility;
+  }
+
+  navbarStyle() {
+    if (screen.width >= 1024) {
+      return{'min-height': 'auto', 'max-height': '50px'};
+    } else if (this.navbarVisibility) {
+      const height = document.getElementsByClassName('navbar-top-level')[0].children.length * 40 + 'px';
+      return {'min-height': height, 'max-height': 'auto'};
+    } else {
+      return {'min-height': '0', 'max-height': '0'};
+    }
   }
 
 }
